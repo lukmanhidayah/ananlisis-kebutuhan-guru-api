fb5beade9524c5cb04094f8ce59399feaaa6a6bb
Lukman Hidayah <45559675+lukmanhidayah@users.noreply.github.com>
2025-06-24 08:08:30 +0700

    Add API version prefix

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ExampleApiController;
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
-Route::get('/example', [ExampleApiController::class, 'index']);
+Route::prefix('v1')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+    Route::get('/example', [ExampleApiController::class, 'index']);
+});
 
